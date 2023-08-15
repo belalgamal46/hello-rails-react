16024a254101d3fea990a221a42749ecffad42d2
theabroadwolf <belalgamal00@gmail.com>
2023-08-15 21:03:34 +0300

    Add react dom to use react and create App component

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,11 @@
 // Entry point for the build script in your package.json
-import "@hotwired/turbo-rails"
-import "./controllers"
+import '@hotwired/turbo-rails';
+import "./controllers";
+
+import React from 'react';
+import {createRoot} from 'react-dom/client';
+import App from './src/app'
+
+createRoot(document.getElementById('root')).render(
+  <App/>
+);
---
+++ app/javascript/src/app.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+
+function App() {
+  return (
+    <div className='container'>
+      <h1>Random Greeting Message App</h1>
+    </div>
+  );
+}
+
+export default App;
