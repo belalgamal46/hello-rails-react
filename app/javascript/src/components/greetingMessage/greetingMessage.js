25905dc6ce6c1a5de21a1d5d454ecafc562076c9
theabroadwolf <belalgamal00@gmail.com>
2023-08-15 23:05:14 +0300

    Utilize redux and add greeting message

--- app/javascript/src/components/greetingMessage/greetingMessage.js
+++ app/javascript/src/components/greetingMessage/greetingMessage.js
@@ -1,10 +1,29 @@
-import React from 'react';
+import React, {useEffect} from 'react';
+import {useDispatch, useSelector} from "react-redux";
+import {fetchRandomMessage} from "../../redux/message/messageSlice";
 
 
 const GreetingMessage = () => {
+  const dispatch = useDispatch();
+  const {message, isLoading} = useSelector((state) => state.message)
+  const greeting = message.greeting_message
+
+  useEffect(() => {
+    dispatch(fetchRandomMessage());
+  }, []);
+
+
+  if (isLoading) {
+    return (
+      <div className="container">
+        <h1>Loading...</h1>
+      </div>
+    );
+  }
+
   return (
     <div className="container">
-      <h1>Greetings</h1>
+      <h1>{greeting}</h1>
     </div>
   );
 }
