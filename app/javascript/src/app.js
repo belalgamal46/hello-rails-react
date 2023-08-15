5ab34dd57e3a8bcc0214b459a66f6b3c493aebc3
theabroadwolf <belalgamal00@gmail.com>
2023-08-15 23:26:46 +0300

    Consume react router

--- app/javascript/src/app.js
+++ app/javascript/src/app.js
@@ -1,10 +1,13 @@
 import React from 'react';
-
+import {Link} from "react-router-dom";
 
 function App() {
   return (
     <div className='container'>
       <h1>Random Greeting Message App</h1>
+      <Link to="/randomGreeting">
+        <button type="button">Greeting</button>
+      </Link>
     </div>
   );
 }
