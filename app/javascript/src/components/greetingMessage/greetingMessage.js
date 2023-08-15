9fad74a343b9aac353535ddb4b3003a2f4391a13
theabroadwolf <belalgamal00@gmail.com>
2023-08-15 23:03:21 +0300

    Create greeting message component

---
+++ app/javascript/src/components/greetingMessage/greetingMessage.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+
+const GreetingMessage = () => {
+  return (
+    <div className="container">
+      <h1>Greetings</h1>
+    </div>
+  );
+}
+
+export default GreetingMessage;
